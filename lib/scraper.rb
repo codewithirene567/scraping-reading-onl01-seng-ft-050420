47595403b8395875e54b1c3cf7452d08ecff89c2
Irene Scott <irenejinscott@gmail.com>
2020-05-23 13:49:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+#html = open("https://flatironschool.com/") #grabs the HTML that makes up the flatiron school's landing page
+#Nokogiri::HTML(html) #converts the string into a nodeset
+#doc = Nokogiri::HTML(html)
+ 
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
